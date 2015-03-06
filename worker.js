0cbafe91a1fdbbf0fcc6cd08dec61c5cf206c975
Lucas Lazaro <user.email>
2015-03-06 18:24:17 -0300

    Acknowledging that message was receive before committing offset

--- worker.js
+++ worker.js
@@ -9,7 +9,8 @@ var moment = require('moment-timezone'),
 
     // database
     mongojs = require('mongojs'),
-    db = mongojs(process.env.MONGO_URL || 'localhost:27017/kafka');
+    db = mongojs(process.env.MONGO_URL || 'localhost:27017/kafka'),
+    Offset = kafka.Offset;;
 
 
 var consumer = new HighLevelConsumer(
@@ -18,7 +19,20 @@ var consumer = new HighLevelConsumer(
         { topic: 'my-node-topic' }
     ],
     {
-      groupId: 'worker.js' // this identifies consumer and make the offset consumption scoped to this key
+      groupId: 'worker.js', // this identifies consumer and make the offset consumption scoped to this key
+      // Auto commit config
+      autoCommit: false,
+      autoCommitIntervalMs: 5000,
+      // The max wait time is the maximum amount of time in milliseconds to block waiting if insufficient data is available at the time the request is issued, default 100ms
+      fetchMaxWaitMs: 100,
+      // This is the minimum number of bytes of messages that must be available to give a response, default 1 byte
+      fetchMinBytes: 1,
+      // The maximum bytes to include in the message set for this partition. This helps bound the size of the response.
+      fetchMaxBytes: 1024 * 10,
+      // If set true, consumer will fetch message from the given offset in the payloads
+      fromOffset: false,
+      // If set to 'buffer', values will be returned as raw buffer objects.
+      encoding: 'utf8'
     }
 );
 
@@ -49,6 +63,17 @@ consumer.on('message', function (message) {
       console.log('MONGO error updating document: ' + err);
     } else {
       console.log('MONGO updating document OK:' + message.value);
+      
+      // Commit the offset after the message is acknowledge
+      consumer.commit(function(err, data) {
+        if(err) {
+          console.log('KAFKA committing the offset error: ' + err);
+        } else {
+          console.log('KAFKA committing sucess:' + data);
+        }
+
+        fetchCommits('worker.js', 'my-node-topic', 0);
+      });
     }
   });
 });
@@ -63,3 +88,23 @@ process.on('beforeExit', function(code) {
     console.log('KAFKA consumer commit ok :)');
   });
 });
+
+// Offset setup to check offset of curent 
+var offset = new Offset(client);
+
+offset.on('ready', function() {
+  console.log('Zookeper ready');
+});
+
+offset.on('connect', function() {
+  console.log('Broker ready');
+});
+
+var fetchCommits = function (groupId, topicName, partition) {
+  offset.fetchCommits(groupId, [
+        { topic: topicName, partition: partition }
+    ], function (err, data) {
+        console.log('Fetch Commits from ' + consumerName + ' group:' + groupId + ', topic:' + topicName + ', partition:' + partition);
+        console.log(data);
+  });
+};
